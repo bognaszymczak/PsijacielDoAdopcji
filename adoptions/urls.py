c1ad619b73a71959e831e575f55fd2b1b7c707dd
bognaszymczak <bogna.szymczak@gmail.com>
2026-01-11 14:21:34 +0100

    Implementation of user authentication: login, logout, and registration features

--- adoptions/urls.py
+++ adoptions/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dog_list, name='dog_list'),
     path('<int:dog_id>/', views.dog_detail, name='dog_detail'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 from django.shortcuts import render, get_object_or_404
 from .models import Dog
 
@@ -8,3 +11,8 @@ def dog_list(request):
 def dog_detail(request, dog_id):
     dog = get_object_or_404(Dog, id=dog_id)
     return render(request, 'dog_detail.html', {'dog': dog})
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
